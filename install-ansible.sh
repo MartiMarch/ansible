974b585a08739703f287ca321940890847c013b4
root <root@localhost.localdomain>
2022-08-27 11:20:08 +0200

    Script de bash para instalar ansible

---
+++ install-ansible.sh
@@ -0,0 +1,9 @@
+yum install -y openssh-server openssh-clients
+systemctl start sshd
+systemctl enable sshd
+yum install -y epel-release ansible
+yum update -y
+ssh-keygen
+ssh-copy-id root@10.0.2.15
+echo '[servidor]' >> /etc/ansible/hosts
+echo '10.0.2.15' >> /etc/ansible/hosts
